7aa09159baa2057420c7023cd0d498ff3cd78968
alexcfyung <alexcfyung@hotmail.com>
2016-01-23 12:16:54 -0500

    Add device disabling GameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public GameObject[] cameras;
+	public float terminateInv;
+	private float currInv;
+	private int index;
+
+	// Use this for initialization
+	void Start () {
+		terminateInv = 5.0f;
+		currInv = terminateInv;
+		index = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currInv > 0) {
+			currInv -= Time.deltaTime;
+		} else {
+			ShutdownCamera();
+			currInv = terminateInv;
+		}
+	}
+
+	void ShutdownCamera() {
+		if (index < cameras.Length) {
+			MeshRenderer rend = cameras[index].GetComponent<MeshRenderer> ();
+			rend.material.SetColor ("_Color", Color.red);
+			index++;
+		} else {
+			// Gameover here
+		}
+	}
+}
